8367eabe18e1eabe863c82ac66c2600f60140d60
Tobias <affiliatemarketer662@gmail.com>
2026-07-10 18:21:25 +0930

    Build.bat file updated minimally

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 if exist build rmdir /S /Q build
 if exist dist rmdir /S /Q dist
 
-pyinstaller --clean --onefile --name TWR_Macro main.py
+PyInstaller --clean --onefile --name TWR_Macro main.py
 
 xcopy "data" "dist\data" /E /I /Y
 
